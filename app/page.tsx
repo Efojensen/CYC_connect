3609e6782f2c3e602460bf3e2e4245983c662d96
EfoJensen <Jensena231@gmail.com>
2026-01-07 18:17:28 +0000

    made mobile view of infoTile and creativeCardTile

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { creativeCardsInfo, events, formations, stories } from '@constants/prime
 
 export default function Home() {
   return (
-    <main className='flex flex-col mt-17.5 px-27 items-center'>
+    <main className='flex flex-col mt-17.5 px-5 md:px-27 items-center'>
       {/* The first large image */}
       <FirstImage
         image='/images/coliseum.png'
@@ -19,41 +19,31 @@ export default function Home() {
 
       <section className='flex flex-col w-full'>
         <SectionTitle title='Events' />
-        <div className='flex gap-6'>
-          {
-            events.map((event, index) => (
-              <InfoTile
-                key={index}
-                id={event.id}
-                alt={event.alt}
-                title={event.title}
-                imgUrl={event.imgUrl}
-                previewContent={event.previewContent}
-              />
-            ))
-          }
+
+        <div className='flex gap-6 overflow-x-auto md:overflow-x-visible flex-nowrap md:flex-wrap no-scrollbar'>
+          {events.map((event, index) => (
+            <div key={index} className='shrink-0'>
+              <InfoTile {...event} />
+            </div>
+          ))}
         </div>
       </section>
+
 
       <section className='flex flex-col w-full my-38'>
         <SectionTitle title='Formations' />
-        <div className='flex gap-6'>
-          {
-            formations.map((formation, index) => (
-              <InfoTile
-                key={index}
-                id={formation.id}
-                alt={formation.alt}
-                title={formation.title}
-                imgUrl={formation.imgUrl}
-                previewContent={formation.previewContent}
-              />
-            ))
-          }
+
+        <div className='flex gap-6 overflow-x-auto md:overflow-x-visible flex-nowrap md:flex-wrap no-scrollbar'>
+          {formations.map((formation, index) => (
+            <div key={index} className='shrink-0'>
+              <InfoTile {...formation} />
+            </div>
+          ))}
         </div>
       </section>
 
-      <section className='flex flex-col relative w-screen px-27 h-111.25 md:h-166.5 bg-specialBlueColoredSection pt-27.75'>
+
+      <section className='flex flex-col relative w-screen px-5.75 md:px-27 h-134.75 md:h-166.5 bg-specialBlueColoredSection pt-11.25 md:pt-27.75'>
         <Image
           width={280}
           height={287}
@@ -72,40 +62,27 @@ export default function Home() {
           title='Stories'
           className='relative z-10'
         />
-        <div className='flex gap-x-6 z-1'>
-          {
-            stories.map((story, index) => (
-              <InfoTile
-                key={index}
-                id={story.id}
-                alt={story.alt}
-                title={story.title}
-                imgUrl={story.imgUrl}
-                authorInfo={story.authorInfo}
-                previewContent={story.previewContent}
-              />
-            ))
-          }
+        <div className='flex gap-x-6 z-1 overflow-x-auto md:overflow-x-visible no-scrollbar'>
+          {stories.map((story, index) => (
+            <div key={index} className='shrink-0'>
+              <InfoTile {...story} />
+            </div>
+          ))}
         </div>
       </section>
 
       <section className='flex flex-col w-full my-17.5'>
         <SectionTitle title='Creative Space' />
-        <div className='flex gap-6'>
-          {
-            creativeCardsInfo.map((creativeCardInfo, index) => (
-              <CreativeCard
-                key={index}
-                src={creativeCardInfo.src}
-                alt={creativeCardInfo.alt}
-                likes={creativeCardInfo.likes}
-                title={creativeCardInfo.title}
-                views={creativeCardInfo.views}
-              />
-            ))
-          }
+        {/* overflow-x-auto md:overflow-x-visible flex-nowrap md:flex-wrap */}
+        <div className='flex gap-6 no-scrollbar overflow-x-auto md:overflow-x-visible'>
+          {creativeCardsInfo.map((creativeCardInfo, index) => (
+            <div key={index}>
+              <CreativeCard {...creativeCardInfo} />
+            </div>
+          ))}
         </div>
       </section>
+
     </main>
   );
 }
